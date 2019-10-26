c52b258ea900a909d84fdd86d10ff4fc7bd0924e
Olsen <olsencar@oregonstate.edu>
2019-10-26 02:15:54 -0700

    modified:   Aggregator/rss_feed_reader.py 	Aggregator/run_rss_reader.bat

--- News Aggregator/Aggregator/rss_feed_reader.py
+++ News Aggregator/Aggregator/rss_feed_reader.py
@@ -1,41 +1,72 @@
 import feedparser
 import json
-import csv
+import hashlib
+import boto3
 
 CATEGORIES = ["politics"]        
 
-# Checks to see if list contains value
-def contains(list, val):
-    for item in list:
-        if (item == val):
-            return True
-    return False
+dynamodb = boto3.resource('dynamodb')
+
+def generate_hash(source, title, published):
+    combined_str = "{}{}{}".format(source, title, published)
+    hash_object = hashlib.sha512(combined_str.encode())
+    hex_digest = hash_object.hexdigest()
+    return hex_digest
 
 def parse_feed(source_name, feed_info):
     try:
         feed = feedparser.parse(feed_info["url"])
+        stories = []
 
-        with open('./stories.csv', 'a', newline='') as stories:
-            for item in feed.entries:
-                writer = csv.writer(stories, delimiter='|', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                writer.writerow([source_name, item['title'], item['description'], item['link'], item['published']])
-
+        for item in feed['entries']:
+            if ('published' in item):
+                stories.append(
+                    {
+                        'source': source_name, 
+                        'title': item['title'], 
+                        'description': item['description'], 
+                        'link': item['link'], 
+                        'orig_link': item['id'],
+                        'publish_date': item['published'],
+                        'article_id': generate_hash(source_name, item['title'], item['published'])
+                    }
+                )
     except Exception as error:
-        print(error)
-        return
+        print(error.with_traceback())
+        exit()
+    return stories
 
 
 def main():
+    data_to_push = []
+
     with open("./rss_feed_config.json", "r") as rss_feeds:
         data = json.load(rss_feeds)
-        
         # for each source in the data sources
         for source in data["sources"]:
             source_name = source["source"]
+            # for each feed in the source
             for feed in source["feeds"]:
                 # If feed category exists in the constant CATEGORIES list, then get the RSS feed
-                if (contains(CATEGORIES, feed['category'])):
-                    parse_feed(source_name, feed)
+                if (CATEGORIES.__contains__(feed["category"])):
+                    data_to_push.extend(parse_feed(source_name, feed))
+
+    table = dynamodb.Table('news_stories')
+
+    with table.batch_writer() as batch:
+        
+        for story in data_to_push:
+            batch.put_item(
+                Item={
+                    'article_id': story['article_id'],
+                    'source': story['source'], 
+                    'title': story['title'], 
+                    'description': story['description'], 
+                    'link': story['link'],
+                    'orig_link': story['orig_link'], 
+                    'publish_date': story['publish_date']
+                }
+            )
 
 if __name__ == "__main__":
     main()          
