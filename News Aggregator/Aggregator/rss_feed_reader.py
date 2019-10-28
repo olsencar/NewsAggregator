47637a6d91979c21db00696fa9bca6f568c139a0
Olsen <olsencar@oregonstate.edu>
2019-10-27 17:10:16 -0700

    Fixed an issue where the description stored didn't contain special chars or numbers

--- News Aggregator/Aggregator/rss_feed_reader.py
+++ News Aggregator/Aggregator/rss_feed_reader.py
@@ -7,17 +7,98 @@ from decimal import Decimal
 import re
 import requests
 from lxml.html import parse
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+from sklearn.feature_extraction.text import TfidfTransformer
 
 CATEGORIES = ["politics"]        
 
 # Start DynamoDB resource
 dynamodb = boto3.resource('dynamodb')
 
+#****************************************************
+# EXTRACTION OF KEYWORDS
+#****************************************************
+# ** Credit to https://www.freecodecamp.org/news/how-to-extract-keywords-from-text-with-tf-idf-and-pythons-scikit-learn-b2a0f3d7e667/
+# ** for the code and explanation of how to implement this
+
+# pre process the text to remove unnecessary characters and words
+def pre_process(text):
+    # remove html tags
+    text = remove_html(text)
+
+    #remove special characters
+    text = re.sub("(\\d)+"," ",text)
+
+    # convert to lowercase
+    text = text.lower()
+    return text
+
 # Removes html from the description
 # Used in keyword searching so it doesn't look inside HTML
 def remove_html(text):
-    return re.search(r"([^<]+)", text).group(1)
+    tmp = re.sub("<[^>]*>", "", text)
+    tmp = re.sub(r"[\(\n)+\(\t)+]+", "", tmp)
+    return tmp
+
+# generate the tf_idf for all of the documents
+# returns the transformer, feature names and the count vectorizer
+def generate_tf_idf(docs):
+    cv = CountVectorizer(max_df=0.85, stop_words=ENGLISH_STOP_WORDS, max_features=10000)
+    word_count_vector = cv.fit_transform(docs)
+    transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
+    transformer.fit(word_count_vector)
+    feature_names = cv.get_feature_names()
+
+    return (transformer, feature_names, cv)
+
+# Sorts a coo matrix based on the score it received    
+def sort_coo(coo_matrix):
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
+# Extracts the top (n) keywords from a list of keywords
+def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+    sorted_items = sorted_items[:topn]
+
+    scores = []
+    feature_vals = []
+
+    for idx, score in sorted_items:
+        # Keep track of feature name and its corresponding score
+        scores.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+
+    #create a tuples of feature,score
+    #results = zip(feature_vals,score_vals)
+    results= {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]]=scores[idx]
+    
+    return results
+
+# Returns a list of keywords for a specific document
+def get_keywords(cv, transformer, feature_names, text):
+    # generate tf_idf for the text
+    tf_idf_vector = transformer.transform(cv.transform([text]))
+
+    # sort the tf_idf vectors by desc order of scores
+    sorted_items = sort_coo(tf_idf_vector.tocoo())
+
+    # extract only the top 10
+    keywords = extract_topn_from_vector(feature_names, sorted_items, 15)
+
+    # Convert keywords array into something useful in DynamoDB
+    kw_obj_arr = []
+    for k in keywords:
+        kw_obj_arr.append(
+            {
+                'keyword': k,
+                'score': Decimal(keywords[k]).__round__(3)
+            }
+        )
+
+    return kw_obj_arr
 
 # Generates the 512 character source, title and url
 def generate_hash(source, title, url):
@@ -31,20 +112,17 @@ def parse_feed(source_name, feed_info):
         feed = feedparser.parse(feed_info["url"])
         stories = []
 
+        docs = []
         for item in feed['entries']:
+            # only parse if the item has a publish date
             if ('published' in item):
-                rake = Rake() # start nlp 
+                # pre process the description to remove unnecessary characters
+                desc_for_kw_processing = pre_process(item['description'])
+
+                # remove the html tags from the description
+                # this description is used in the UI, so keep case and special characters in it.
                 desc = remove_html(item['description'])
-                rake.extract_keywords_from_text(desc)
-                
-                keywords = []
-                for kw in rake.get_ranked_phrases_with_scores():
-                    keywords.append(
-                        {
-                            'keyword': kw[1],
-                            'score': Decimal(str(kw[0]))
-                        }
-                    )
+
                 stories.append(
                     {
                         'source': source_name, 
@@ -52,11 +130,17 @@ def parse_feed(source_name, feed_info):
                         'description': desc,
                         'link': item['link'], 
                         'orig_link': item['id'],
-                        'keywords': keywords,
+                        'keywords': [],
                         'publish_date': item['published'],
                         'article_id': generate_hash(source_name, item['title'], item['link'])
                     }
                 )
+                docs.append(desc_for_kw_processing)
+        
+        (transformer, feature_names, cv) = generate_tf_idf(docs)
+        for story in stories:
+            story['keywords'] = get_keywords(cv, transformer, feature_names, story['description'])
+
     except Exception as error:
         print(error.with_traceback())
         exit()
