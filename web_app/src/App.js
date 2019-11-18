6ab9b747c9f4824c8971709b10f686f44228edc0
sharky <aaa.borkar@gmail.com>
2019-11-17 22:45:27 -0800

    created very basic feed pulling data from json file

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Article from './components/Article.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          News Aggregator
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.data
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <p>
+            News Aggregator
+          </p>
+        </header>
+        {this.state.data.map((article, idx)=>{
+          return <Article key={idx} title={this.state.data[idx].title} img={this.state.data[idx].img} author={this.state.data[idx].author} content={this.state.data[idx].content} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
