08c2f8251b284f396697b56ac5fa1f28c00f5d0d
sharky <aaa.borkar@gmail.com>
2020-02-09 20:16:42 -0800

    updated PostComment function, fetching and uploading comments work now 2

--- web_app/routes/articleRoutes.js
+++ web_app/routes/articleRoutes.js
@@ -49,19 +49,7 @@ module.exports = (app) => {
         //search_primary_key and search_secondary_key
         let s_primary_id = comment_data.body.primary_id;
         let s_secondary_id = comment_data.body.secondary_id;
-
-        //////////////////////////////////////////////////////////////////////////////////////
-        //using upsert
         let new_comment = comment_data.body.group_comments[0]
-
-        //READ THIS NOTE: 
-        // I need the filter below to be an OR statement like this:
-        // { $or:[{ primary_id: s_primary_id, secondary_id: s_secondary_id}, { primary_id: s_secondary_id, secondary_id: s_primary_id}]}
-        // to find comment documents even if the articles are in different order/structuring
-        // it looks like if upsert (findOneandUpdate) doesnt find a document, it constructs one using the 'filter' + 'update'
-        // BUT if you try to use the filter above with the OR like I was talking about it just doesnt use it in the construction of the new doc (= new doc wont have the)
-        // primary_id and secondary_id fields... so right now I have just hard coded the filter to always assume correct document id ordering when looking in the db.
-
         const filter = {primary_id: s_primary_id, secondary_id: s_secondary_id};
         const update = { $push: { group_comments: new_comment } };
         let doc = await Comment.findOneAndUpdate(filter, update, {
@@ -70,45 +58,8 @@ module.exports = (app) => {
             useFindAndModify: false
         });
         return res.status(200).send(doc);
-        //////////////////////////////////////////////////////////////////////////////////////
-        
-        //const comment_group = await Comment.findOne({ $or:[{ primary_id: s_primary_id, secondary_id: s_secondary_id}, { primary_id: s_secondary_id, secondary_id: s_primary_id}]}); 
-        //console.log(comment_group);
-        // if (!comment_group) { //comment not found -> create new doc with this comment
-        //     console.log("creating entirely new comment group document");
-        //     console.log(comment_data.body);
-        //     // creating new document
-        //     // const singelton = new Comment(comment_data.body);
-        //     // await singelton.save(function(err) {
-        //     //     if (err) { return res.status(500).send(); }
-        //     //     next();
-        //     // });
-        //     await Comment.create(comment_data.body, function (err, doc) {
-        //         if(err){
-        //             console.log("Failed to create new comment_group document for first comment in article group");
-        //             return res.status(500).send();
-        //         }
-        //         else{
-        //             console.log("Successfully inserted comment");
-        //             return res.status(200).send(doc);
-        //         }
-        //     });
-        // }
-        // else{
-        //     //update and save the comment_group document with the newest comment appended to its array
-        //     comment_group.group_comments.push(comment_data.body.group_comments[0]); //note: the comment data is passed in in the
-        //                                                                     //form of a mongo comment document to make this and 'create' code easier                                      
-        //     await comment_group.save(function(err) {
-        //         if (err) { return res.status(500).send(); }
-        //         next();
-        //     });
-        //     //success
-        //     console.log("1. Succesfully inserted comment");
-        //     return res.status(200).send();
-        // }
     });
 
-    
     app.get('/api/articles/recent', async (req, res) => {
         let beginDate = new Date();
         beginDate.setDate(beginDate.getDate() - 7);
