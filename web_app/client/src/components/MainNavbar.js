4b7fd0ce6bc54c12a02bc077a08a9861151db730
Olsen <olsencar@oregonstate.edu>
2020-02-11 22:47:57 -0800

    Improved styling of the navbar and increased margin of articles container

--- web_app/client/src/components/MainNavbar.js
+++ web_app/client/src/components/MainNavbar.js
@@ -7,13 +7,13 @@ class MainNavbar extends Component {
     render() {
         return (
             <Navbar collapseOnSelect sticky="top" expand="sm" bg="red-blue-gradient" variant="dark">
-                <Navbar.Brand href="/">PURPLE</Navbar.Brand>
+                <Navbar.Brand href="/">PURPLE NEWS</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-center">
-                    <div className="wrap">
+                    <div className="search-bar-wrapper">
                         <div className="search-container">
                             <InputGroup>
-                                <FormControl type="text" className="searchTerm" placeholder="Search" />
+                                <FormControl type="text" className="searchTerm" placeholder="Search..." />
                                 <InputGroup.Append>
                                     <button type="submit" className="searchButton">
                                         <FontAwesomeIcon icon={faSearch} />
