2bc7f4910dc1a66ef24f1defe1f5b4b76e8ba568
Olsen <olsencar@oregonstate.edu>
2020-02-13 09:32:10 -0800

    Added an implementation of Universal Sentence Encoder

---
+++ Training/trainingUSE.py
@@ -0,0 +1,93 @@
+import re
+import tensorflow as tf
+tf.get_logger().setLevel('ERROR')
+import tensorflow_hub as hub
+import numpy as np
+import os, sys
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+import urllib
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+tf.compat.v1.enable_eager_execution()
+
+#performs the cosine similarity between two vectors
+def cos_sim(vectors):
+    return cosine_similarity(vectors)
+
+def get_most_sim(sentence, compare_sentences, sim_matrix, topN=6):
+    index = compare_sentences.index(sentence)
+    sim_row = np.array(sim_matrix[index, :])
+    # print(sim_row)
+    indices = sim_row.argsort()[-topN:][::-1][1:]
+    # print(sim_row.argsort())
+    # print(sim_row.argsort()[-topN:][::-1][1:])
+   
+    return [{'description': compare_sentences[i], 'similarity_score': sim_row[i]} for i in indices]
+
+# Removes html from the description
+# Used in keyword searching so it doesn't look inside HTML
+def remove_html(text):
+    tmp = re.sub("<[^>]*>", "", text)
+    tmp = re.sub(r"[\(\n)+\(\t)+]+", "", tmp)
+    return tmp
+
+# Opens the mongoDB client connection
+def openMongoClient():
+    # connectionDetails.json is formatted as follows:
+    # {
+    #     "user": <USERNAME>
+    #     "password": <PASSWORD>
+    # }   
+    with open("connectionDetails.json", "r") as conn:
+        config = json.load(conn)
+        user = urllib.parse.quote(config['user'])
+        pwd = urllib.parse.quote(config['password'])
+        return MongoClient("mongodb+srv://{}:{}@newsaggregator-0ys1l.mongodb.net/test?retryWrites=true&w=majority".format(user, pwd))
+
+
+def get_articles(client, days_back=10):
+    """
+
+    Gets articles from the past `days_back` to now from MongoDB
+
+    `client` : A `MongoClient` object that has been instantiated.
+
+    `days_back` :
+        This parameter defines how many days back to search in the DB. This is 7 by default.
+
+    Returns:
+        A List of articles as triplets (_id, description, publish_date)
+    """
+    coll = client['NewsAggregator'].news_stories
+    items = []
+
+    for item in coll.find(
+        {"publish_date": {"$gte": datetime.utcnow() - timedelta(days=days_back) } }, { "description": 1, 'publish_date': 1 }
+        ).sort('publish_date', -1):
+        # Add the item to the dictionary
+        if ('description' not in item):
+            print(item)
+        else:
+            items.append(item['description'])
+    return items
+
+def main():        
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+    embed = hub.load(module_url)
+    client = openMongoClient()
+    docs = get_articles(client)
+    sentences_embeddings = embed(docs)
+
+    sim_matrix = cos_sim(np.array(sentences_embeddings))
+    sentence = docs[0]
+    print("SENTENCE: " + sentence)
+    most_sim = get_most_sim(sentence, docs, sim_matrix)
+    print("MOST SIMILAR")
+    
+    for x in range(len(most_sim)):
+        print(most_sim[x])
+
+if __name__ == "__main__":
+    main()
+
