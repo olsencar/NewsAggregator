f4d12497628421c6bd0f29b23b68827e0bb4af3b
Carter Olsen <olsencar@oregonstate.edu>
2020-03-27 17:42:26 -0700

    Removed HTML Entities from articles

--- web_app/client/src/components/Article.js
+++ web_app/client/src/components/Article.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-// the actual article component takes in props from the article_data file and returns JSX with that data
-// uses bootstraps card content box to format article
+
 class Article extends Component {
     getPartyColor(bias) {
         switch (bias) {
@@ -15,13 +14,22 @@ class Article extends Component {
             default:
                 return "mid";
         }
+    };
+
+    removeHTMLFromString = (html) => {
+        let elem = document.createElement('div');
+        elem.innerHTML = html;
+        const text = elem.textContent;
+        elem.textContent = '';
+        return text;
     }
+
     render() {
         return (
             <div className="article card bg-light rounded">
                 <div className="card-body">
-                    <h5 className="card-title"><a className="card-title-a" href={this.props.link} target="_blank" rel="noopener noreferrer">{this.props.title}</a></h5>
-                    <p className="card-text">{this.props.content}</p>
+                    <h5 className="card-title"><a className="card-title-a" href={this.props.link} target="_blank" rel="noopener noreferrer">{this.removeHTMLFromString(this.props.title)}</a></h5>
+                    <p className="card-text">{this.removeHTMLFromString(this.props.content)}</p>
                 </div>
                 <div className="card-footer text-muted">
                     <span className={"card-link banner-source banner-source-" + this.getPartyColor(this.props.bias)}>{this.props.source}</span>
