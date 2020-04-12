252cf148b6dca4083d468baa6db20a985603a9b2
carterols <cjolsen06@gmail.com>
2020-04-11 18:29:14 -0700

    Added icon for each comment

--- web_app/client/src/components/Comment.js
+++ web_app/client/src/components/Comment.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react'
 import Media from 'react-bootstrap/Media'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faImage } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 class Comment extends Component {
     render() {
       const commentTime = new Date(this.props.time).toLocaleString("en-US")
       return (
         <Media as="li">
-            {this.props.profilePic ? (
-              <img
-              width={64}
-              height={64}
-              className="mr-3"
-              src={this.props.profilePic}
-              alt=""
-            />) : (
-              <FontAwesomeIcon icon={faImage} size='lg' color='grey' />
-            )}
-            <div className="text-left">
+            <FontAwesomeIcon icon={faUser} size='lg' color='grey' />
+            <div className="text-left ml-2">
               <strong className="text-success mt-0">{this.props.user}</strong>
               <span className="text-muted float-right">
                   &nbsp;
