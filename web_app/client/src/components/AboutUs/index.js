da887a691ff0392080d65ff792f81f191ae1949f
Race <stewarra@oregonstate.edu>
2020-05-04 12:45:20 -0700

    updated about page to include more detail & chart

--- web_app/client/src/components/AboutUs/index.js
+++ web_app/client/src/components/AboutUs/index.js
@@ -15,18 +15,22 @@ const AboutUs = () => {
               <hr />
               <Card.Text>
                 As accessibility to online news sources increases, so does the extremity of confirmation
-                bias and echo-chambers. Purple News was began and driven by a passion to fight these issues
-                within modern media.
+                bias and echo-chambers. Popular media outlets intentionally bias content for user satisfaction.
+                Purple News was began and driven by a passion to fight these issues within modern media. Purple News
+                scrapes popular news sites and couples politically polarized articles on the same topic for a simple, intuitive
+                viewing experience. This allows the user to both visualize the bias in their news and expand their horizons
+                in terms of the differing perspectives and ideas they take in.
                 <br />
                 <br />
                 The bias level that we apply to each source is taken from <a href="https://www.allsides.com/media-bias/media-bias-chart" target="_blank" rel="noopener noreferrer">this</a> media bias chart by AllSides. Allsides has performed extensive research to determine the bias level of each of these sources.
               </Card.Text>
+              <img src="https://www.allsides.com/sites/default/files/AllSidesMediaBiasChart_Version1.1_11.18.19.jpg" class="BiasChart"/>
             </Card.Body>
           </Card>
         </Col>
       </Row>
     </Container>
-    
+
   );
 }
 
